7d5c46419f37a7f64f5feea49b45cc5e14bb8f79
onuryil09 <onuryil09@gmail.com>
2024-10-02 15:17:52 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea22 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend22 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series22 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea23 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend23 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series23 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea24 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend24 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series24 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(484, 327);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(484, 682);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(893, 682);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(604, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 60);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(756, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 60);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "KES";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(604, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(601, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // chart1
+            // 
+            chartArea22.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea22);
+            legend22.Name = "Legend1";
+            this.chart1.Legends.Add(legend22);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series22.ChartArea = "ChartArea1";
+            series22.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series22.Legend = "Legend1";
+            series22.Name = "Series1";
+            this.chart1.Series.Add(series22);
+            this.chart1.Size = new System.Drawing.Size(553, 312);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // chart2
+            // 
+            chartArea23.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea23);
+            legend23.Name = "Legend1";
+            this.chart2.Legends.Add(legend23);
+            this.chart2.Location = new System.Drawing.Point(12, 367);
+            this.chart2.Name = "chart2";
+            series23.ChartArea = "ChartArea1";
+            series23.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series23.Legend = "Legend1";
+            series23.Name = "Series1";
+            this.chart2.Series.Add(series23);
+            this.chart2.Size = new System.Drawing.Size(553, 312);
+            this.chart2.TabIndex = 8;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea24.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea24);
+            legend24.Name = "Legend1";
+            this.chart3.Legends.Add(legend24);
+            this.chart3.Location = new System.Drawing.Point(616, 367);
+            this.chart3.Name = "chart3";
+            series24.ChartArea = "ChartArea1";
+            series24.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series24.LabelBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            series24.Legend = "Legend1";
+            series24.Name = "Series1";
+            this.chart3.Series.Add(series24);
+            this.chart3.Size = new System.Drawing.Size(508, 312);
+            this.chart3.TabIndex = 9;
+            this.chart3.Text = "chart3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(601, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(601, 270);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(601, 308);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 682);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(275, 16);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Kalp Hızı Değişkenliği (Heart Rate Variability)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 327);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(265, 16);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Dakikadaki Vuruş Sayısı (Beats Per Minute)";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(613, 682);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(83, 16);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Nefes Sayısı";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 707);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Reflection.Emit;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp2
+{
+
+    
+    public partial class Form1 : Form
+    {
+        long b;     // b= BPM
+        long h;     // h= HRV
+        long n;     // n= Nefes
+        long minm1 = 0, maxm1 = 30;
+        long minm2 = 0, maxm2 = 30;
+        long minm3 = 0, maxm3 = 30;
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //serialPort1.BaudRate = 9600;
+        }
+
+        private string data;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.Open();
+                timer1.Start();
+                button1.Enabled = false;
+                button2.Enabled = true;
+                label4.Text = "Bağlantı açık";
+                label4.ForeColor = Color.Green;
+                chart1.Series[0].Color = Color.Red;     //line color = red
+                chart2.Series[0].Color = Color.Green;   //line color = green
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ("hata:"));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            string[] ports = SerialPort.GetPortNames();                 //Port isimlerini çekiyoruz
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);                              //comboBox a port isimlerini ekledik
+            }
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);        // bu event i datalarımızı almak için yapıyoruz
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                data = serialPort1.ReadLine();                              //data çekme işlemi
+                this.Invoke(new EventHandler(displaydata));                 // bu dataları progress bar ve label a yazdırmak için Invoke oluşturuyoruz
+            }
+            catch (Exception ex)
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                }
+                serialPort1.Open();
+
+            }
+             //System.Threading.Thread.Sleep(100);             //!!!  Veri Okuma İşlemine Zaman Tanı:
+        }
+
+        private void displaydata(object sender, EventArgs e)
+        {
+            
+
+                string[] value = data.Split(',');
+
+                if (value[0] == "START")
+                {
+                    try
+                    {
+                        b = Convert.ToInt32(value[1]);          //BPM
+                    }
+                    catch (Exception ex)
+                    {
+                        b = 0;
+                        //throw;
+                }
+                    try
+                    {
+                        h = Convert.ToInt32(value[2]);          //HRV
+                    }
+                    catch (Exception ex)
+                    {
+                        h = 0;
+                        //throw;
+                }
+                    try
+                    {
+                        n = Convert.ToInt32(value[3]);          //NEFES   
+                    }
+                    catch (Exception ex)
+                    {
+                        n = 0;
+                        //throw;
+                    }
+
+                
+                    
+                }
+
+            
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+                timer1.Stop();
+                button1.Enabled = true;
+                button2.Enabled = false;
+                label4.Text = "Bağlantı kapalı";
+                label4.ForeColor = Color.Red;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ("hata: "));
+
+            }
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(serialPort1.IsOpen) 
+            { 
+                serialPort1.Close(); 
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            label1.Text = "BPM: " + b;
+            label2.Text = "HRV: " + h;
+            label3.Text = "Nefes: " + n;
+
+            label5.Text = Convert.ToString(b);
+            label6.Text = Convert.ToString(h);
+            label7.Text = Convert.ToString(n);
+
+
+            
+            
+                chart1.ChartAreas[0].AxisX.Minimum = minm1;
+                chart1.ChartAreas[0].AxisX.Maximum = maxm1;
+           
+                chart1.ChartAreas[0].AxisY.Minimum = 0;
+                chart1.ChartAreas[0].AxisY.Maximum = 250;
+
+                chart1.ChartAreas[0].AxisX.ScaleView.Zoom(minm1, maxm1);
+
+                if (b != null)
+                {
+                    this.chart1.Series[0].Points.AddXY((maxm1 + minm1) / 2, b);                 
+                    maxm1++;
+                    minm1++;
+                }
+
+
+            chart2.ChartAreas[0].AxisX.Minimum = minm2;
+            chart2.ChartAreas[0].AxisX.Maximum = maxm2;
+
+            chart2.ChartAreas[0].AxisY.Minimum = 0;
+            chart2.ChartAreas[0].AxisY.Maximum = 150;
+
+            chart2.ChartAreas[0].AxisX.ScaleView.Zoom(minm2, maxm2);
+
+            if (h != null)
+            {
+                this.chart2.Series[0].Points.AddXY((maxm2 + minm2) / 2, h);
+                maxm2++;
+                minm2++;
+            }
+
+
+            chart3.ChartAreas[0].AxisX.Minimum = minm3;
+            chart3.ChartAreas[0].AxisX.Maximum = maxm3;
+
+            chart3.ChartAreas[0].AxisY.Minimum = 0;
+            chart3.ChartAreas[0].AxisY.Maximum = 5;
+
+            chart3.ChartAreas[0].AxisX.ScaleView.Zoom(minm3, maxm3);
+
+            if (n != null)
+            {
+                this.chart3.Series[0].Points.AddXY((maxm3 + minm3) / 2, n);
+                maxm3++;
+                minm3++;
+            }
+        
+            serialPort1.DiscardInBuffer();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
